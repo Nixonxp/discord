106be80b1a6e400dfd34aad7438392ca1e3d3c42
Ilya Nikiforov <nixonxp@mail.ru>
2024-04-29 12:22:36 +0300

    init

---
+++ Makefile
@@ -0,0 +1,18 @@
+.PHONY: build
+build:
+	@printf '\033[36mСборка контейнеров\033[0m\n'
+	@docker compose build
+
+.PHONY: up
+up: ## (пере)сборка образов и запуск приложения
+	@printf '\033[36mЗапуск контейнеров\033[0m\n'
+	@docker compose up -d
+
+.PHONY: down
+down: ## остановка приложения
+	@docker compose down
+
+.PHONY: restart
+restart: ## перезапуск приложения
+	@docker compose down
+	@docker compose up -d
---
+++ chat/cmd/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"math/rand/v2"
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.HTML(http.StatusOK, "Chat service response")
+	})
+
+	e.GET("/health", func(c echo.Context) error {
+		status := http.StatusOK
+		statusMessage := "OK"
+
+		if !isServiceOk(10) {
+			status = http.StatusInternalServerError
+			statusMessage = "Error"
+		}
+
+		return c.JSON(status, struct{ Status string }{Status: statusMessage})
+	})
+
+	e.GET("/ready", func(c echo.Context) error {
+		status := http.StatusOK
+		statusMessage := "OK"
+
+		if !isServiceOk(5) {
+			status = http.StatusInternalServerError
+			statusMessage = "Error"
+		}
+
+		return c.JSON(status, struct{ Status string }{Status: statusMessage})
+	})
+
+	httpPort := os.Getenv("PORT")
+	if httpPort == "" {
+		httpPort = "8080"
+	}
+
+	e.Logger.Fatal(e.Start(":" + httpPort))
+}
+
+// isServiceOk в зависимости от входящего значения вернет false, например
+// передано 5, тогда (100 / 5 = 20) 20% вероятностью вернется false, для теста сервиса
+func isServiceOk(probability int) bool {
+	randNumber := rand.IntN(probability-1) + 1
+
+	if randNumber == 1 {
+		return false
+	}
+
+	return true
+}
